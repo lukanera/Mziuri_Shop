d7bc6048d2ff04a50dd785160a7424d3c1b819a7
luka gagnidze <lukagag09@gmail.com>
2024-12-07 19:06:36 +0400

    add product list, detail_product

---
+++ mziuri_shop/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Product
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products':products})
+
+def product_detail(request):
+    return render(request, 'product_detail.html')
+
