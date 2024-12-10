be117d65124c3067981993eadfdb1a391f2fe88c
luka gagnidze <lukagag09@gmail.com>
2024-12-10 22:20:56 +0400

    Your commit message

--- mziuri_shop/products/views.py
+++ mziuri_shop/products/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
 
-def product_detail(request):
-    return render(request, 'product_detail.html')
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    product.views += 1
+    product.save()
+    return render(request, 'product_detail.html', {"product": product})
 
